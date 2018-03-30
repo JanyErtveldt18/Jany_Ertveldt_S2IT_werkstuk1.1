1c4e1f42a9907b286d497e7c9d365baf79c50bfd
Jany Ertveldt <jany.ertveldt@student.ehb.be>
2018-03-30 23:01:00 +0200

    Initial Commit

---
+++ Werkstuk1_Jany_Ertveldt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werkstuk1_Jany_Ertveldt
+//
+//  Created by Jany Ertveldt on 30/03/18.
+//  Copyright © 2018 Jany Ertveldt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
